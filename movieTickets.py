a289c5970511b28e45277a135990871d4f86b5cf
16033jhc <16033@jhc.school.nz>
2021-02-17 10:44:22 +1300

    Added movieTickets.py file

---
+++ movieTickets.py
@@ -0,0 +1,37 @@
+# Imports
+
+# Functions
+def not_blank(question):
+    valid = False
+    while not valid:
+        response = input(question)
+        if response != "":
+            return response
+        else:
+            print("Sorry - this can't be blank.")
+
+###### Main Routine ######
+
+# Dictionaries / Lists for data storage
+
+# Offer first time users a walkthrough of the program
+
+# Loop for ticket details
+
+    # Name (can't be blank)
+        name = not_blank("Name: ")
+    # Age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop for snacks
+
+    # Calculate snack price
+
+    # Payment Method (and add surcharge for credit card)
+
+# Math for total price
+
+# Profit?
+
+# Data Output to file
