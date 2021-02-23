b453e9c2e9132995f55254c203ee979422ea9f8c
16033jhc <16033@jhc.school.nz>
2021-02-23 13:28:26 +1300

    File updated

--- movieTickets.py
+++ movieTickets.py
@@ -1,14 +1,17 @@
 # Imports
 
 # Functions
-def not_blank(question):
-    valid = False
-    while not valid:
-        response = input(question)
-        if response != "":
-            return response
-        else:
-            print("Sorry - this can't be blank.")
+#question = "What is your name?\n"
+#def not_blank(question):
+#    valid = False
+#
+#    while not valid:
+#        response = input(question)
+#
+#        if response != "":
+#            return response
+#        else:
+#            print("Sorry - this can't be blank.")
 
 ###### Main Routine ######
 
@@ -19,7 +22,10 @@ def not_blank(question):
 # Loop for ticket details
 
     # Name (can't be blank)
-        name = not_blank("Name: ")
+        while True:
+            name = input("What is your name?\nName: ")
+
+
     # Age (between 12 and 130)
 
     # Calculate ticket price
